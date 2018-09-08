6441af03dba0f42767d4987cd16f170fe088162f
Nick <nicholas_gregory@hotmail.com>
2018-09-08 17:10:33 -0400

    Updates for Mobile

--- server.js
+++ server.js
@@ -8,24 +8,27 @@ const apiRouter = require('./routes/people');
 const app = express();
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://gregornc:administrator1@ds231205.mlab.com:31205/friend-list', { promiseLibrary: require('bluebird') })
-.then(() =>  console.log('connection successful'))
+mongoose.connect('mongodb://gregornc:administrator1@ds231205.mlab.com:31205/friend-list', {promiseLibrary: require('bluebird')})
+.then(() => console.log('connection successful'))
 .catch((err) => console.error(err));
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '/dist/FriendList')));
 app.use('/', express.static(path.join(__dirname, '/dist/FriendList')));
 app.use('/api', apiRouter);
 
+//Must be the lat route
+app.use('*', express.static(path.join(__dirname, '/dist/Friendlist')));
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -33,6 +36,13 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.send(err.status);
+});
+
+app.options("*",function(req,res,next){
+  res.header("Access-Control-Allow-Origin", req.get("Origin")||"*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //other headers here
+  res.status(200).end();
 });
 
 const port = process.env.PORT || '8080';
