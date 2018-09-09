b5645156f3732a2d83d45bdb47f055d2fb36b461
Nick <nicholas_gregory@hotmail.com>
2018-09-09 11:15:32 -0400

    Updates for CORS

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const createError = require('http-errors');
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const logger = require('morgan');
 
 const apiRouter = require('./routes/people');
 
 const app = express();
+
+app.use(cors());
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://gregornc:administrator1@ds231205.mlab.com:31205/friend-list', {promiseLibrary: require('bluebird')})
@@ -36,13 +39,6 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.send(err.status);
-});
-
-app.options("*",function(req,res,next){
-  res.header("Access-Control-Allow-Origin", req.get("Origin")||"*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  //other headers here
-  res.status(200).end();
 });
 
 const port = process.env.PORT || '8080';
